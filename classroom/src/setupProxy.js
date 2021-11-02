ee98c02a408ab58bc93595be6a4ae26e2a65c7c1
tdp2k <phuoc.tran@globee.hk>
2021-11-02 23:49:48 +0700

    connect server (5)

--- classroom/src/setupProxy.js
+++ classroom/src/setupProxy.js
@@ -1,5 +1,5 @@
-const proxy = require("http-proxy-middleware");
-
-module.exports = app => {
-  app.use(proxy("/", { target: "https://btcn3-18127186-server.herokuapp.com" }));
+const { createProxyMiddleware } = require('http-proxy-middleware');
+     
+module.exports = function(app) {
+    app.use(createProxyMiddleware('/', { target: 'https://btcn3-18127186-server.herokuapp.com' }));
 };
