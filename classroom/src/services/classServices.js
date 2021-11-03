59d9aecd47eddedf46326c62650a04b038cb3b75
tdp2k <phuoc.tran@globee.hk>
2021-11-03 09:50:21 +0700

    connect server (9)

--- classroom/src/services/classServices.js
+++ classroom/src/services/classServices.js
@@ -1,7 +1,10 @@
 export async function getAllClasses() {
 
     try{
-        const response = await fetch('/classes');
+        const response = await fetch('https://btcn3-18127186-server.herokuapp.com/classes', {
+            method: 'GET',
+            headers: {'Content-Type': 'application/json'}
+        });
         return await response.json();
     }catch(error) {
         return [];
@@ -10,7 +13,7 @@ export async function getAllClasses() {
 }
 
 export async function createClass(data) {
-    const response = await fetch(`/classes`, {
+    const response = await fetch(`https://btcn3-18127186-server.herokuapp.com/classes`, {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({name: data})
